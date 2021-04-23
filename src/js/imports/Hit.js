43d8df0977e051d8cb6a02bfb2761435c5e984e7
Ravi <raviteja.valluri@rackspace.com>
2021-04-23 11:51:32 -0700

    Seaech Fix

--- src/js/imports/Hit.js
+++ src/js/imports/Hit.js
@@ -6,11 +6,23 @@ import Snippet from './Snippet';
 function HeaderLink(props) {
   const headerUrl = props.headerUrl;
   if (headerUrl.includes('/docs/')) {
-    return (<a className="search-type-link" href="/docs/">Solution Docs</a>);
+    return (<a className="search-type-link" href="/docs/">Solutions</a>);
   } else if (headerUrl.includes('/support/how-to/')) {
     return ( <a className="search-type-link" href="/support/how-to/">How-To</a>);
   } else if (headerUrl.includes('/blog/')) {
     return (<a className="search-type-link" href="/blog">Blog</a>);
+  } else {
+    return (<a className="search-type-link" href="/">Developer Home</a>);
+  }
+}
+function TitleTag(props) {
+  const headerUrl = props.headerUrl;
+  if (headerUrl.includes('/docs/')) {
+    return (<a className="search-tag technical-tag" href="/docs/">Technical</a>);
+  } else if (headerUrl.includes('/support/how-to/')) {
+    return ( <a className="search-tag article-tag" href="/support/how-to/">Article</a>);
+  } else if (headerUrl.includes('/blog/')) {
+    return (<a className="search-tag post-tag" href="/blog">Post</a>);
   } else {
     return (<span></span>);
   }
@@ -20,9 +32,9 @@ function CategoryLink(props) {
   if (categories != '' && categories != null) {
     if (Array.isArray(categories)) {
       categories = categories.flat();
-      return (categories.map((category) => <a key={category} className="search-type-link">{category.replace(/ *\([^)]*\) */g, "")}{index < hit.categories.length - 1 ? ',\u00A0' : ''}</a>))
+      return (<span>&nbsp;&gt;&nbsp;{categories.map((category) => <a key={category} className="search-type-link">{category.replace(/ *\([^)]*\) */g, "")}{index < hit.categories.length - 1 ? ',\u00A0' : ''}</a>)}</span>)
     } else {
-      return (<a className="search-type-link">{categories}</a>);
+      return (<span>&nbsp;&gt;&nbsp;<a className="search-type-link">{categories}</a></span>);
     }
   } else {
     return (<span></span>);
@@ -33,8 +45,9 @@ const Hit = ({ hit }) => {
     return (
       <div className="row">
         <div className="col-sm-12">
-          <HeaderLink headerUrl={hit.url} />&nbsp;&gt;&nbsp;<CategoryLink category={hit.category} />
+          <HeaderLink headerUrl={hit.url} /><CategoryLink category={hit.category} />
           <h2>
+            <TitleTag headerUrl={hit.url} />
             <a className="search-title-link" href={`${hit.url}`}>
               <Highlight attribute="title" hit={hit} />
             </a>
