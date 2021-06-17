de39e50a4d50e2d8bee98750df0a0112ed13170d
Ravi <raviteja.valluri@rackspace.com>
2021-06-17 13:26:02 -0700

    update

--- crawler.js
+++ crawler.js
@@ -196,7 +196,7 @@ const crawl = async (startUrl, baseUrl) => {
         await writeFile(searchIndexPath, stringifiedIndex);
         console.log(
           `Search Index JSON generated at ${chalk.cyan(
-            `/${jsonFileName}.json`
+            `${searchIndexPath}/${jsonFileName}.json`
           )}!`
         );
       }
